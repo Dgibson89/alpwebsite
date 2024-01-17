8382a3e58c0bba4fb780be11c1cf54f144257032
Dgibson89 <donovan.gibson89@gmail.com>
2024-01-17 15:48:32 -0600

    Removed Fade out effect

--- script.js
+++ script.js
@@ -12,20 +12,6 @@ $('#imageCarousel').carousel({
   });
   
 
-  document.addEventListener('DOMContentLoaded', function () {
-    document.querySelectorAll('a').forEach(function (link) {
-        link.addEventListener('click', function (e) {
-            e.preventDefault(); // Prevent the default link behavior
-            var destination = this.href;
-
-            document.body.classList.add('fade-out');
-
-            setTimeout(function () {
-                window.location.href = destination;
-            }, 1000); // This timeout duration should match the CSS transition time
-        });
-    });
-});
 
 document.addEventListener('DOMContentLoaded', function () {
     document.body.classList.add('fade-in');
